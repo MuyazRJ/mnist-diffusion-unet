57748a4c538c1c799d028eee918c8f4a5ebd9bf4
Muyaz Rahman <Muyaz154@Gmail.com>
2026-03-26 13:57:42 +0000

    Add author and project information to unet.py

--- models/unet.py
+++ models/unet.py
@@ -1,4 +1,8 @@
-# models/unet.py
+# Author: Mohammed Rahman
+# Student ID: 10971320
+# University of Manchester — BSc Computer Science Final Year Project, 2026
+#
+
 import torch
 import torch.nn as nn
 
